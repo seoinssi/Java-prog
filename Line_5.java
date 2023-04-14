902eebdf89835bb10be757704524eabc511ae963
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-14 15:09:14 +0900

    Update Line_5.java

--- Line_5.java
+++ Line_5.java
@@ -10,10 +10,10 @@ class Line_5 extends Point_5 {
 	int get_a() { if (a < 0) return 0; else return a; }
 	int get_b() { if (b < 0) return 0; else return b; }
 
-	public int get_x() {
-		if ( super.get_x() > 0) return (super.get_x());
-		else return (0);
-	}
+	public int get_x() { //public을 사용해야 어디서든 접근가능함을 의미
+		if ( super.get_x() > 0) return (super.get_x());//컴파일 오류 발생
+		else return (0);//override가 안된 이유는 상위 클래스에서 멤버변수 접근x
+	}//상위 클래스는 Point_5임
 	public int get_y() {
 		if ( super.get_y() > 0) return (super.get_y());
 		else return (0);
