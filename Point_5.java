cb0c43d51a84dbd6bb0017f24f5e7b5b70ee0e79
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-14 15:11:07 +0900

    Add files via upload
    원래 코든 private를 사용했는데, 컴파일 실행을 위해 그 부분을 public이 아닌 protected로 수정함.
    protected로 수정한 이유는 캡슐화가 깨질 수 있기 때문임. 
    
    캡슐화는 객체의 상태(필드)와 행동(메서드)을 캡슐 형태로 묶어 외부로부터의 직접적인 접근을 제한하여, 객체의 내부 구현을 감추고 객체 간의 결합도를 낮추는 것을 목적으로 합니다. 따라서 필드를 public으로 선언하면, 외부에서 직접 필드에 접근하여 값을 변경할 수 있으므로 캡슐화 원칙을 위반하게 됩니다.
    
    protected 접근 제어자는 같은 패키지 내에서는 접근이 가능하며, 서브클래스에서도 접근이 가능하다는 특징이 있습니다. 이를 통해 서브클래스에서 부모 클래스의 필드와 메서드에 접근할 수 있게 되면서, 캡슐화를 유지하면서도 상속 관계를 통해 필요한 정보를 제공할 수 있습니다.
    
    따라서 Point_5 클래스의 x 필드를 protected로 유지하는 것이 객체 지향 프로그래밍의 원칙을 따르는 것이며, public으로 변경할 경우에는 캡슐화가 깨지게 되므로 주의해야 합니다.

---
+++ Point_5.java
@@ -0,0 +1,13 @@
+package kor.point;
+
+public class Point_5 {
+	protected int x;
+	protected int y;
+
+	public Point_5(int x, int y) { 
+		this.x = x; this.y = y;
+	}
+
+	public int get_x() { return x; }
+	public int get_y() { return y; }
+}
