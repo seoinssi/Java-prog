c6a2f89ab603e056e756b6c3d18f6ba52b8994ac
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-18 02:27:47 +0900

    Create Output.java
    배열 표기 [] 를 배열 원소 타입에 붙이는 것이 좋다.
    배열 자체가 하나의 내부 클래스이기 때문에 원소 타입 + 배열이 최종 타입이 됨
    
    몇개의 인수가 있는지는 배열의 크기 피드(멤버 변수)인 배열.length 값에서 얻을 수 있고 c언어와 달리 자바 실행 파일명 자체는 인수에 포함되지 않음.
    
    system.out은 이미 컴파일되어 system.out.class 형태로 내부 어딘가에 저장되어있는 내장(시스템) 클래스
    어떤 클래스들이 어디에 저장되는가는 나중에 패키지라는 주제에서 다룸

---
+++ Output.java
@@ -0,0 +1,14 @@
+class Output {
+    public static void main(String args[]) {
+
+        int i;
+
+        System.out.print("Output test...\n");
+        System.out.print("Output test...");
+        System.out.print(args.length);
+        System.out.print("%d\n", args.length);
+
+        for (i = 0; i < args.lengt; i++) 
+            System.out.println(args[i]);                       
+    }
+}
