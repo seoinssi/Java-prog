06f3070c6791b53d10f0e50cb8e493e799d32194
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-18 02:38:54 +0900

    Create Input.java
    system.in.read() 메소드는 키보드에서 입력된 문자 즉, 문자 코드를 바이트 단위의 정수로 간주해 하나씩 돌려준다
    
    영문 알파벳은 한 바이트 아스키코드로 한글은 두 바이트 자바 유니코드로 해석
    한글은 두번에 걸쳐 한 바이트씩 정수로 얻을 수 있음
    
    숫자 아스키 코드 - '0'은 해당 숫자에 대응되는 수치

---
+++ Input.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Input {
+    public static void main(String args[]) trows IOException {
+        int x, y, z;
+
+        x = System.in.read();
+        y = System.in.read();
+        z = (x- '0') + (y - '0');
+
+        System.out.printf("x=%c, %d(%x), y=%c, $d(%x)\n", x,x,x y,y,y);
+
+        System.out.printf("%c+%c=%d\n", x, y, z);
+    }
+}
