8b342ed428facacdace8320aaf336694131cd684
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 08:42:03 +0900

    Create Add_1.java

---
+++ STEP_07(class package)/W1(소스 파일 계층적 분리 재조명)/Add_1.java
@@ -0,0 +1,16 @@
+class Add_1 {
+    int total;
+    int add2c(int a, int b) {
+        int s;
+        s = a + b;
+        total += s;
+        return(s);
+    }
+
+    float(float a, float b) {
+        float s;
+        s = a + b; 
+        total += s;
+        retrun(s);
+    }
+}
