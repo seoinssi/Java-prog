eafdd187fdaead8e56d51294d02a88b8e3dc2484
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 09:39:06 +0900

    Create Point_4.java

---
+++ STEP_08/W4(접근제한모드)/Point_4.java
@@ -0,0 +1,16 @@
+package point;
+
+public class Point_4 {
+	private int x;	
+	private int y;
+	public Point_3(int x, int y) {
+		this.x = x; this.y = y;
+	}
+
+	public int get_x() {
+		return x;
+	}
+	public int get_y() {
+		return y;
+	}
+}
