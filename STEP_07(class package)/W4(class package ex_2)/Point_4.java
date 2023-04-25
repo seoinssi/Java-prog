f15ab677741ceb21315b75bfb5d3b0f8b964cebf
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 09:19:27 +0900

    Create Point_4.java

---
+++ STEP_07(class package)/W4(class package ex_2)/Point_4.java
@@ -0,0 +1,14 @@
+package kor.ptln;
+
+/*public*/ class Point_4 {
+	/*public*/ int x;
+	/*public*/ int y;
+
+	/*public*/ Point_4(int x, int y) { this.x = x; this.y = y;}
+
+	void set_xy(int x, int y) {this.x = x; this.y = y;}
+	void set_x(int x) {this.x = x;}
+	void set_y(int y) {this.y = y;}
+	int get_x() {return x;}
+	int get_y() {return y;}
+}	
