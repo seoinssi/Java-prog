df62f3b1ea530b32d57adb61b7c3f4c7b4b34f03
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 09:23:46 +0900

    Create STEP_07_PointLine_5.java

---
+++ STEP_07(class package)/W4(참조 패키지 클래스명 단축 표기)/STEP_07_PointLine_5.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+import kor.ptln.*;
+
+class STEP_07_PointLine_5 {
+
+	public static void main(String args[]) throws IOException {
+		
+		Line_5 line;
+		int x, y, a, b;
+		double dl;
+
+		x = System.in.read() - '0'; y = System.in.read() - '0';
+		a = System.in.read() - '0'; b = System.in.read() - '0';
+
+		line = new Line_5(x, y, a, b);
+	
+		dl = line.get_dl();
+		System.out.printf("dl=%f\n", dl);
+	}	
+}
+		
