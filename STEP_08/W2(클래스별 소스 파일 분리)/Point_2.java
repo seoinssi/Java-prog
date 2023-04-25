7a84305eba37f6ca7a23c876fbd60fb6b725f9c0
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 09:32:51 +0900

    Create Point_2.java

---
+++ STEP_08/W2(클래스별 소스 파일 분리)/Point_2.java
@@ -0,0 +1,13 @@
+class Point_2 {
+    int x;
+    int y;
+    Point_1(int x, int y) {
+        this.x = x; this.y = y;
+    }
+    int get_x() {
+        return x;
+    }
+    int get_y() {
+        return y;
+    }
+}
