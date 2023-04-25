79dced32acc81385109a8455ef5a57f61fd2746f
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 09:37:32 +0900

    Create Point_3.java

---
+++ STEP_08/W3/Point_3.java
@@ -0,0 +1,16 @@
+//package point;
+
+class Point_3 {
+	int x;	
+	int y;
+	Point_3(int x, int y) {
+		this.x = x; this.y = y;
+	}
+
+	int get_x() {
+		return x;
+	}
+	int get_y() {
+		return y;
+	}
+}
