4cd6f0cb9c578f7b873dd248347ed19c9eddc994
Jeong seoin <120116251+seoinssi@users.noreply.github.com>
2023-04-25 09:16:21 +0900

    Create Point_3.java

---
+++ STEP_07(class package)/W3(class package ex_1)/Point_3.java
@@ -0,0 +1,14 @@
+package point;
+
+public class Point_3 {
+	public int x;
+	public int y;
+
+	public Point_3(int x, int y) { this.x = x; this.y = y;}
+
+	void set_xy(int x, int y) {this.x = x; this.y = y;}
+	void set_x(int x) {this.x = x;}
+	void set_y(int x) {this.y = y;}
+	int get_x() {return x;}
+	int get_y() {return y;}
+}	
